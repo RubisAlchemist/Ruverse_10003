e6e1b0a83f33a2a0694da157f47135727b818ae7
soyeons <luvisallaround@naver.com>
2024-10-30 15:48:40 +0900

    server change

---
+++ src/store/ai/aiConsultSlice.js
@@ -0,0 +1,156 @@
+import { ruverseClient } from "@apis/ruverse";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  audio: {
+    sonny: {
+      defaultSrc: "https://ruverse-10003.com/video/default_sonny.mp4",
+      greetingsSrc: "https://ruverse-10003.com/video/greetings_sonny.webm",
+      errorSrc: "https://ruverse-10003.com/video/pardon_sonny.webm",
+      noteSrc: "https://ruverse-10003.com/video/note_sonny.mp4",
+    },
+    karina: {
+      defaultSrc: "https://ruverse-10003.com/video/default_karina.mp4",
+      greetingsSrc: "https://ruverse-10003.com/video/greetings_karina.webm",
+      errorSrc: "https://ruverse-10003.com/video/pardon_karina.webm",
+      noteSrc: "https://ruverse-10003.com/video/note_karina.mp4",
+    },
+    isNotePlaying: false,
+    isGreetingsPlaying: true,
+    src: "",
+    upload: {
+      error: null,
+      isError: false,
+      isSuccess: false,
+      isLoading: false,
+    },
+    current: 1,
+  },
+  modal: {
+    open: false,
+    message: null,
+  },
+};
+
+export const uploadKlleonRequest = createAsyncThunk(
+  "asyncThunk/uploadAudioRequest",
+  async (audioForm) => {
+    const response = await ruverseClient.post(
+      "/counseling/get_klleon_response",
+      audioForm,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    const data = response.data;
+
+    return data;
+  }
+);
+
+export const uploadNewSessionRequest = createAsyncThunk(
+  "uploadNewSession", // Type string
+  async (formData, { rejectWithValue }) => {
+    // Payload creator function
+    try {
+      console.log(formData);
+      const response = await ruverseClient.post("/counseling/init", formData, {
+        // It's better to let Axios set the Content-Type header for FormData
+        // headers: {
+        //   "Content-Type": "multipart/form-data",
+        // },
+      });
+      const data = response.data;
+      console.log("response data: ", response);
+      return data;
+    } catch (error) {
+      console.error("Upload failed:", error);
+      // Optionally, you can return a custom error message
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const uploadRequest = createAsyncThunk(
+  "asyncThunk/uploadAudioRequest",
+  async (audioForm) => {
+    console.log(audioForm);
+    const response = await ruverseClient.post(
+      "/counseling/get_response",
+      audioForm,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    const data = response.data;
+    console.log("response data: ", response);
+    return data;
+  }
+);
+
+export const aiConsultSlice = createSlice({
+  name: "aiConsultSlice",
+  initialState,
+  reducers: {
+    closeModal: (state) => {
+      state.modal = initialState.modal;
+      state.audio.upload = initialState.audio.upload;
+    },
+    clearAudioSrc: (state) => {
+      state.audio.src = initialState.audio.src;
+    },
+    setAudioSrc: (state, action) => {
+      state.audio.src = action.payload;
+    },
+    // 새로운 reducer 추가
+    setGreetingsPlayed: (state) => {
+      state.audio.isGreetingsPlaying = false;
+    },
+    setNotePlaying(state) {
+      state.audio.isNotePlaying = true;
+    },
+    clearNotePlaying(state) {
+      state.audio.isNotePlaying = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(uploadRequest.pending, (state) => {
+      state.audio.upload.isLoading = true;
+    });
+    builder.addCase(uploadRequest.fulfilled, (state, action) => {
+      state.audio.upload.isSuccess = true;
+      state.audio.upload.isLoading = false;
+      state.audio.upload.isError = false;
+      state.audio.upload.error = null;
+
+      state.audio.current += 1;
+      state.audio.src = action.payload;
+      state.modal.open = true;
+      state.modal.message = "요청 성공";
+    });
+    builder.addCase(uploadRequest.rejected, (state, action) => {
+      state.audio.upload.isSuccess = false;
+      state.audio.upload.isLoading = false;
+      state.audio.upload.isError = true;
+      state.audio.upload.error = action.error.message;
+      state.audio.src = "error"; // 📌 src를 'error'로 설정
+      state.modal.message = "요청 실패";
+      state.modal.open = true;
+    });
+  },
+});
+
+export const {
+  clearAudioSrc,
+  closeModal,
+  setAudioSrc,
+  setGreetingsPlayed,
+  setNotePlaying,
+  clearNotePlaying,
+} = aiConsultSlice.actions;
+
+export default aiConsultSlice.reducer;
